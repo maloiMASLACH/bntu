585b96471873813ef70cb8ba7d42b401150b999e
Aliaksey Chyker <aliaksei.chyker@innowise-group.com>
2023-02-07 11:58:52 +0300

    refactor: remove usless files

--- src/constants/labels.ts
+++
@@ -1,9 +0,0 @@
-enum Labels {
-  Email = 'Your E-mail',
-  Name = 'User Name (Login)',
-  Password = 'Password',
-  Confirm = 'Conform Password',
-  NewPassword = 'New Password',
-}
-
-export default Labels;
